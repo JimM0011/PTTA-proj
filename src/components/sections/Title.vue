e1521650540c95209f4bf288ca1b89abb49be2ce
MJ-check <mj188163@163.com>
2025-06-03 22:36:21 +0800

    update

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -126,7 +126,7 @@ const buttons = [
 
     <!-- 作者名单 -->
     <el-row justify="center">
-      <a :href=author.homepage v-for="author in authors">
+      <a :href=author.homepage v-for="author in authors" target="_blank">
         <el-button class="title-button" type="primary" text>
           <el-avatar v-if="author.icon" :size="40" :src="author.icon" />
           <span class="author">
@@ -138,7 +138,7 @@ const buttons = [
 
     <!-- 地址名单 -->
     <el-row justify="center">
-      <a :href=address.homepage v-for="address in addresses">
+      <a :href=address.homepage v-for="address in addresses" target="_blank">
         <el-button class="title-button" type="primary" text>
           <el-avatar v-if="address.icon" :size="40" :src="address.icon" />
           <span class="address">
