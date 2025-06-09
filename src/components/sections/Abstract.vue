9e52b8a957f3b6f5b159fa15ebae8830d590a7df
MJ-check <mj188163@163.com>
2025-06-09 23:18:15 +0800

    update

--- src/components/sections/Abstract.vue
+++ src/components/sections/Abstract.vue
@@ -73,7 +73,7 @@ export default {
         </el-col>
         <el-col :xs="24" :sm="20" :md="8" :lg="7" :xl="7" class="abstract-img-container">
             <img 
-                src="./abstract.jpg"
+                :src="'./abstract.jpg'"
                 alt="Pipeline"
                 class="abstract-img"
             >
